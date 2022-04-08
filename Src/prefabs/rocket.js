8b83564be8ff2fb99e44bde29ab35ba8c2868520
Jacob Dickerman <jwdicker@ucsc.edu>
2022-04-08 15:34:50 -0700

    Added firing & resetting

--- Src/prefabs/rocket.js
+++ Src/prefabs/rocket.js
@@ -2,6 +2,44 @@ class Rocket extends Phaser.GameObjects.Sprite {
     
     constructor(scene, posX, posY, texture) {
         super(scene, posX, posY, texture);
-        scene.add.existing(this)
+        scene.add.existing(this);
+
+        this.initX = posX;
+        this.initY = posY;
+
+        this.firing = false;
+    }
+
+    update() {
+        const movementSpeed = 4;
+        if(keyLEFT.isDown) {
+            this.x -= movementSpeed;
+        }
+
+        if(keyRIGHT.isDown) {
+            this.x += movementSpeed;
+        }
+
+        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.firing = true;
+        }
+
+        if(Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.resetPos();
+        }
+
+        if(this.firing) {
+            this.y -= 10;
+
+            if(this.y < 0) {
+                this.resetPos();
+            }
+        }
+    }
+
+    resetPos() {
+        this.firing = false;
+        this.x = this.initX;
+        this.y = this.initY;
     }
 }
--- Src/scenes/play.js
+++ Src/scenes/play.js
@@ -14,8 +14,9 @@ class Play extends Phaser.Scene {
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
-
         this.background = this.add.tileSprite(0, 0, game.config.width, game.config.height, "starfield").setOrigin(0,0);
+
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431, "rocket").setOrigin(0.5, 0);
 
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00ff00).setOrigin(0,0);
 
@@ -23,24 +24,10 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xffffff).setOrigin(0,0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, game.config.height, 0xffffff).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize, 0, game.config.width, game.config.height, 0xffffff).setOrigin(0,0);
-
-        this.p1Rocket = new Rocket(this, game.config.width/2, 431, "rocket").setOrigin(0.5, 0);
     }
 
     update() {
         this.background.tilePositionX -= 3;
-
-        const movementSpeed = 4;
-        if(keyLEFT.isDown) {
-            this.p1Rocket.x -= movementSpeed;
-        }
-
-        if(keyRIGHT.isDown) {
-            this.p1Rocket.x += movementSpeed;
-        }
-
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
-            debugger;
-        }
+        this.p1Rocket.update();
     }
 }
