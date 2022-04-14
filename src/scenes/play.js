fe0b94c29a7cccacaf71a348b3b9fa0075ecda2d
Jacob Dickerman <jwdicker@ucsc.edu>
2022-04-14 16:00:01 -0700

    Reorganize Asset Files

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -7,7 +7,7 @@ class Play extends Phaser.Scene {
         // Images
         this.load.image("rocket", "assets/rocket.png");
         this.load.image("spaceship", "assets/spaceship.png");
-        this.load.image("starfield", "assets/starfield.png");
+        this.load.image("starfield", "assets/backgrounds/starfield.png");
 
         // Explosion Animation
         this.load.spritesheet("explosion", "assets/explosion.png", {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
